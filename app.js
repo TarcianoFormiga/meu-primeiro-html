635ebb9a43ac8c42ced696d94c4d2510b85200c6
TARCINO <tarcianoformiga@gmail.com>
2022-09-27 15:56:07 -0300

    utilizando operadores

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Meu primeiro código Javascript")
+
+const soma = 10 + 10;
+const multi = 10 * 10;
+const divi = 10 / 10;
+
+console.log("Soma =", soma);
+
+console.log("Multiplicação =", multi);
+
+console.log("Divisão de 10 / 10 =", divi);
